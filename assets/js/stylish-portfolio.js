8d7d77f829702c671e1a927bcbb7f89613ba727c
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-11-24 21:01:38 -0500

    add all necessary images to assets folder

--- assets/js/stylish-portfolio.js
+++ assets/js/stylish-portfolio.js
@@ -9,6 +9,10 @@
     $(this).toggleClass("active");
   });
 
+  $('portfolio-item').hover(function() {
+    console.log($(this));
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
